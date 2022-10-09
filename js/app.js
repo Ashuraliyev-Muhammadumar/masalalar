44c728673acf3440d3fc3fcf885b917500c322d6
Ashuraliyev-Muhammadumar <ashiraliyevmuhammadumar@gmail.com>
2022-10-10 02:09:59 +0500

    misol

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let age = prompt("sonni kiriting");
+function convert() {
+    if(age%2 == 0){
+        console.log("juft son => Yes");
+    }else console.log("toq son => No");             
+
+}
+convert();
